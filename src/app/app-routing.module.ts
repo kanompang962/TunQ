8ccd560f4ea2be1066def3ab9e1855640543a2a2
tiencahi chamnan <kanompang962@gmail.com>
2023-10-23 16:46:54 +0700

    test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { path: '', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
   { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
   { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
   { path: 'queue', loadChildren: () => import('./queue/queue.module').then(m => m.QueueModule) }];
